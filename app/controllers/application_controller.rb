d3a74204148d1f1d20128b8dcb7b77640f9e0741
Benjamin Cheng <bencheng200@gmail.com>
2021-01-08 16:29:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params[:email], password: params[:password])
-    puts params
+    session[:user_id] = @user.id
     if @user
       session[:user_id] = @user.id
       redirect '/users/home'
